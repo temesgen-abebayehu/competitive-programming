0629df122c26f9934a7ede72313c3c4a3f7dc0f1
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-02-11 09:14:56 +0300

    Time: 382 ms (52.87%) | Memory: 46.4 MB (74.95%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -3,10 +3,14 @@ class Solution:
         product = defaultdict(int)
         ans = 0
 
+        # add the product to dictionary
         for first in range(len(nums)):
             for second in range(first + 1, len(nums)):
                 product[nums[first] * nums[second]] += 1
+
+        # cout the valid tuples
         for key, value in product.items():
             if value > 1:
                 ans += (value*(value - 1)//2) * 8
+
         return ans
