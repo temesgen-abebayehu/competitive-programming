1de453015e6c65a3fffea71c753f352559c3c542
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-02-21 14:55:56 +0300

    Time: 99 ms (37.61%) | Memory: 44.8 MB (54.03%) - LeetSync

--- 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
+++ 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
         nums.sort(reverse=True)
-        max_num = len(nums) + 1
-        
-        prefix = [0] * (max_num + 1)
+        prefix = [0] * (len(nums) + 1)
+
         for i, j in requests:
             prefix[i] += 1
             if j < len(prefix) - 1:
@@ -18,4 +17,3 @@ class Solution:
             ans += (prefix[i] * nums[i])
                 
         return ans%(10**9 + 7)
-            
