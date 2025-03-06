f1706a4976b4eff78e83664afde338f094e7c0d5
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-03-06 19:38:01 +0300

    Time: 4 ms (12.27%) | Memory: 17.8 MB (71.72%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -6,8 +6,6 @@ class Solution:
             city[i] = costs[i][1] - costs[i][0]
 
         city = sorted(city.items(), key=lambda x: x[1])
-        print(city)
-        print(city[0][0])
         a = n//2
         min_cost = 0
         for b in range(n//2):
