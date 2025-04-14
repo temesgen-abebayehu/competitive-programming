1fbb4a25d14f92dc5dee5e38b06845daace5c45b
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-04-14 17:32:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.97%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.py
+++ 1005-univalued-binary-tree/univalued-binary-tree.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        curr = root
         que = deque()
         que.append(root)
 
@@ -14,20 +13,14 @@ class Solution:
             n = len(que)
             for i in range(n):
                 pre = que.popleft()
-                if pre.left and pre.right:
-                    if pre.val != pre.left.val or pre.val != pre.right.val:
-                        return False
 
-                    que.append(pre.left)
-                    que.append(pre.right)
-
-                elif pre.left:
+                if pre.left:
                     if pre.val != pre.left.val:
                         return False
 
                     que.append(pre.left)
 
-                elif pre.right:
+                if pre.right:
                     if pre.val != pre.right.val:
                         return False
 
