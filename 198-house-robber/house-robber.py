0a562bca8fcc5bf5a6c58333f9bf76f41ceaf7f0
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-22 17:06:21 +0300

    Time: 4 ms (3.38%) | Memory: 18.1 MB (12.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,16 +2,17 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         memo = defaultdict(int)
         n = len(nums)
-
+        
+        @cache
         def dp(i):
             if i >= n:
                 return 0
             
-            if i in memo:
-                return memo[i]
+            # if i in memo:
+            #     return memo[i]
             
-            memo[i] = max(nums[i] + dp(i+2), dp(i+1))
+            return max(nums[i] + dp(i+2), dp(i+1))
             
-            return memo[i]
+            # return memo[i]
 
         return dp(0)
