8f48541b1dec69a4b81244cf2158bb46b05d7691
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-22 15:03:44 +0300

    Time: 35 ms (81.74%) | Memory: 17.7 MB (40.75%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+        
+    def fib(self, n: int) -> int:
+        if n == 1 or n == 0:
+            return n
+        
+        if n in self.memo:
+            return self.memo[n]
+        self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        return self.memo[n]
