7e8f9c6c1a545f06dc2c71e4ff6e7ce40dfe2b35
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-23 22:38:05 +0300

    Time: 107 ms (18.27%) | Memory: 17.8 MB (35.77%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
@@ -1,10 +1,14 @@
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
+        nums.sort()
         n = len(nums)
         res = 0
 
         for i in range(n):
             for j in range(n):
+                if nums[j] - nums[i] > nums[i]:
+                    break
+                    
                 if abs(nums[i] - nums[j]) <= min(nums[i], nums[j]):
                     res = max(res, nums[i] ^ nums[j])
 
