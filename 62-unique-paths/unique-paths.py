24771b1a1578c88f1e42e91ea2fe55e45c26aee1
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-24 23:26:16 +0300

    Time: 3 ms (21.18%) | Memory: 18.2 MB (5.88%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,27 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
+
+        def dp(i, j):
+            if i == m-1 and j == n-1:
+                return 1
+
+            if i == m:
+                return 0
+
+            if j == n:
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i , j)]
+
+            path1 = dp(i, j+1)
+            path2 = dp(i+1, j)
+
+            res = path1 + path2
+
+            memo[(i, j)] = res
+            return res
+
+        return dp(0 , 0)
+            
