2657ef1fa4cecd0e55e4c58282df60efc09aaed4
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-25 14:35:42 +0300

    Time: 7 ms (41.45%) | Memory: 19.2 MB (17.24%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+
+        def dp(i, hold):
+            if i < 0:
+                if hold:
+                    return float('-inf')
+                else:
+                    return 0
+
+            if (i, hold) in memo:
+                return memo[(i, hold)]
+
+            # not take
+            res = dp(i-1, hold)
+
+            # take
+            if hold:
+                res = max(res, dp(i-2, not hold)- prices[i])
+
+            else:
+                res = max(res, dp(i-1, not hold) + prices[i])
+
+            memo[(i, hold)] = res
+            return res
+
+        return dp(len(prices)-1, False)
