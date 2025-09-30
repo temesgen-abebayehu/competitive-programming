37c407a52f11f92dc237a5a940e6ddc8e25c1397
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-09-30 11:48:30 +0300

    Time: 6 ms (22.85%) | Memory: 20 MB (11.61%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         memo = {}
-        max_cost = sum(cost)
 
         def dp(i):
             if i == 0:
@@ -12,11 +11,7 @@ class Solution:
             if i in memo:
                 return memo[i]
 
-            # Take
             res =  cost[i] + min(dp(i-1), dp(i-2))
-
-            # # not take
-            # res += dp(i-1)
 
             memo[i] = res
             return res
