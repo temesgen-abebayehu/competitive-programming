ef21ed6389c4d2f4b2ffeae4550c8f1483eb2e9e
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-01 11:22:02 +0300

    Time: 71 ms (75.89%) | Memory: 57.1 MB (92.03%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,20 +1,36 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:        
         n = len(questions)
+        # bottom-up dp
+        dp = [0] * n
+        dp[n-1] = questions[n-1][0]
+
+
+        for i in range(n-1,-1,-1):            
+            if i + questions[i][1] + 1 < n:
+                dp[i] = max(dp[i+1], questions[i][0] + dp[i + questions[i][1] + 1])
+            elif i+1 < n:
+                dp[i] = max(questions[i][0], dp[i+1])
+
+
+        return dp[0]
+
+
+
         # top-dowm dp
 
-        memo = {}
+        # memo = {}
 
-        def dp(i):
-            if i >= n:
-                return 0
+        # def dp(i):
+        #     if i >= n:
+        #         return 0
 
-            if i in memo:
-                return memo[i]
+        #     if i in memo:
+        #         return memo[i]
 
-            res = max(dp(i+1), questions[i][0] + dp(i + questions[i][1] + 1))
+        #     res = max(dp(i+1), questions[i][0] + dp(i + questions[i][1] + 1))
             
-            memo[i] = res
-            return res
+        #     memo[i] = res
+        #     return res
 
-        return dp(0)
+        # return dp(0)
