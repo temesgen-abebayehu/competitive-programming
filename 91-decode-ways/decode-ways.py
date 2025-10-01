f56f6b1a1cc29e7398efc179ce0ddfab84088a92
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-01 15:40:34 +0300

    Time: 3 ms (26.28%) | Memory: 17.8 MB (69.25%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -3,23 +3,36 @@ class Solution:
         # top-down dp
         docode = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'}
 
-        memo = {}
-        def dp(i):
-            if i == len(s) - 1 and s[i] != '0':
-                return 1
-            if i > len(s)-1:
-                return 1
-                
-            if i in memo:
-                return memo[i]
-            res = 0
-            if s[i] in docode:
-                res += dp(i+1)
-            if s[i:i+2] in docode:
-                res += dp(i+2)
+        dp = [0] * (len(s)+1)
+        if s[len(s)-1] != '0':
+            dp[len(s)-1] = 1   
 
-            memo[i] = res
-            return res
-        return dp(0)
+        dp[len(s)] = 1
+
+        for i in range(len(s)-2, -1, -1):
+            if s[i] in docode:
+                dp[i] += dp[i+1]
+            if s[i:i+2] in docode:
+                dp[i] += dp[i+2]
+        return dp[0]
+
+        # memo = {}
+        # def dp(i):
+        #     if i == len(s) - 1 and s[i] != '0':
+        #         return 1
+        #     if i > len(s)-1:
+        #         return 1
+                
+        #     if i in memo:
+        #         return memo[i]
+        #     res = 0
+        #     if s[i] in docode:
+        #         res += dp(i+1)
+        #     if s[i:i+2] in docode:
+        #         res += dp(i+2)
+
+        #     memo[i] = res
+        #     return res
+        # return dp(0)
 
 
