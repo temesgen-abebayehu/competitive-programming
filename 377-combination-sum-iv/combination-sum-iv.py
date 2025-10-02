52e5ffdc787073766be0852dec6c2b2cf023a4d9
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-03 01:07:49 +0300

    Time: 51 ms (14.15%) | Memory: 17.5 MB (99.80%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,22 +1,34 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        # top-down dp
-        memo = {}
-        def dp(curr_target):
-            if curr_target == 0:
-                return 1
+        # bottom-up dp
+        dp = [0] * (target + 1)
+        dp[0] = 1
 
-            if curr_target < 0:
-                return 0
-
-            if curr_target in memo:
-                return memo[curr_target]
-
-            count = 0
+        for i in range(1, target+1):
             for num in nums:
-                count += dp(curr_target - num)
-            
-            memo[curr_target] = count
-            return count
+                if i - num >= 0:
+                    dp[i] += dp[i-num]
 
-        return dp(target)
+        return dp[target]
+
+        
+        # # top-down dp
+        # memo = {}
+        # def dp(curr_target):
+        #     if curr_target == 0:
+        #         return 1
+
+        #     if curr_target < 0:
+        #         return 0
+
+        #     if curr_target in memo:
+        #         return memo[curr_target]
+
+        #     count = 0
+        #     for num in nums:
+        #         count += dp(curr_target - num)
+            
+        #     memo[curr_target] = count
+        #     return count
+
+        # return dp(target)
