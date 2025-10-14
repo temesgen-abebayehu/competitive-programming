dd621092c7ab157063cda34d0bdeb9a8082705c1
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-14 17:18:51 +0300

    Time: 55 ms (40.58%) | Memory: 32.3 MB (31.61%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,42 +1,39 @@
 class TrieNode:
     def __init__(self):
         self.is_end = False
-        self.children = [ None for _ in range(26) ]
+        self.children = {}
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        
+
 
     def insert(self, word: str) -> None:
         curr = self.root
         for c in word:
-            idx = ord(c) - ord('a')
-            if not curr.children[idx]:
-                curr.children[idx] = TrieNode()
-            curr = curr.children[idx]
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
         curr.is_end = True
 
     def search(self, word: str) -> bool:
         curr = self.root
         for c in word:
-            idx = ord(c) - ord('a')
-            if not curr.children[idx]:
+            if c not in curr.children:
                 return False
                 
-            curr = curr.children[idx]
+            curr = curr.children[c]
 
         return curr.is_end
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.root
         for c in prefix:
-            idx = ord(c) - ord('a')
-            if not curr.children[idx]:
+            if c not in curr.children:
                 return False
 
-            curr = curr.children[idx]
+            curr = curr.children[c]
 
         return True
 
