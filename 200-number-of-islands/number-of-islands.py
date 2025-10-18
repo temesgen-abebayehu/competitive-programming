c5d48ee1bd491fd501830567d955fd389ceab5c5
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-18 21:51:11 +0300

    Time: 436 ms (5.22%) | Memory: 31.4 MB (5.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -7,30 +7,22 @@ class Solution:
         # for checking the index is bounded
         def is_bound(r,c):
             return 0 <= r < row and 0 <= c < col
-        
-        neighbor = [[1,0], [0,1], [-1,0], [0,-1]]
 
-        def bfs(i,j):
-            queue = deque([(i,j)])
+        def dfs(i,j):
+            if is_bound(i,j) and grid[i][j] == "1":
+                grid[i][j] = "0"
 
-            # mark as visited
-            grid[i][j] = "0"
-
-            while queue:
-                ci, cj = queue.popleft()
-                for ni, nj in neighbor:
-                    if is_bound(ci+ni, cj+nj) and grid[ci+ni][cj+nj] == "1":
-                        queue.append((ci+ni, cj+nj))
-                        # mark as visited
-                        grid[ci+ni][cj+nj] = "0"
-
+                dfs(i+1, j)
+                dfs(i-1, j)
+                dfs(i, j+1)
+                dfs(i, j-1)
 
         count = 0
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == "1":
                     count += 1
-                    bfs(i,j)
+                    dfs(i,j)
 
         return count
 
