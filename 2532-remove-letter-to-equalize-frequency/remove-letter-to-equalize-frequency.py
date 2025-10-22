077712a89e9f1dee24a299707ef70cb99722ced7
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-22 17:53:20 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (63.88%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -1,15 +1,17 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
         freq = Counter(word)
-        freq_occur = Counter(freq.values())
-        if len(freq_occur) == 1 and (1 in freq_occur or 1 in freq_occur.values()):
+        result = Counter(freq.values())
+        
+        if len(freq) == 1 or (len(result) == 1 and 1 in result.keys()):
             return True
 
-        if len(freq_occur) == 2:
-            if 1 in freq_occur.keys() and freq_occur[1] == 1:
-                return True
-            key = sorted(freq_occur.keys())
-            if key[1] - key[0] == 1 and freq_occur[key[1]] == 1:
-                return True
         
+        if len(result) == 2:
+            key = sorted(list(result.keys()))
+            if key[1] - key[0] == 1 and result[key[1]] == 1:
+                return True
+            if 1 in result.values() and result[1] == 1:
+                return True
+
         return False
