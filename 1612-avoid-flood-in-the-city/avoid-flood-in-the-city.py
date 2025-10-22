db701d728c6241cd7a4fab842651260481d1828d
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-22 10:35:41 +0300

    Time: 373 ms (5.33%) | Memory: 34.1 MB (53.53%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -0,0 +1,22 @@
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        full_lake = {}
+        dry_day = []
+        result  = [-1] * len(rains)
+        for day, lake in enumerate(rains):
+            if lake == 0:
+                dry_day.append(day)
+                result[day] = 1
+            else:
+                if lake in full_lake:
+                    d = bisect_left(dry_day, full_lake[lake])
+                    if d<len(dry_day) and dry_day[d] < day:
+                        result[dry_day[d]] = lake
+                        dry_day.pop(d)
+                    else:
+                        return []
+                full_lake[lake] = day
+
+
+        return result
+            
