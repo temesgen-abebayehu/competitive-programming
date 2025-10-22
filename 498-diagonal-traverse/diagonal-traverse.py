efa872b59f1ec540d0d5a4a793b6fe47b521e276
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-22 13:16:35 +0300

    Time: 6 ms (84.42%) | Memory: 19.7 MB (85.66%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,38 +1,34 @@
-from typing import List
-
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        def inbound(i, j):
-            return 0 <= i < len(mat) and 0 <= j < len(mat[0])
-        
-        direction = 1  # 1 for upward, -1 for downward
-        ans = []
         rows = len(mat)
         cols = len(mat[0])
-        row, col = 0, 0
+        dirc = True  # True = upward and False = downward
+        result = []
+        row = 0
+        col = 0
 
-        for _ in range(rows * cols):
-            ans.append(mat[row][col])
+        for k in range(rows*cols):
+            result.append(mat[row][col])
 
-            # Calculate the next cell in the current direction
-            new_row = row + (-1 if direction == 1 else 1)
-            new_col = col + (1 if direction == 1 else -1)
-
-            if inbound(new_row, new_col):
-                row, col = new_row, new_col
-            else:
-                if direction == 1:
-                    # If we are moving upwards and hit the boundary
-                    if col == cols - 1:
-                        row += 1
-                    else:
-                        col += 1
+            if dirc:
+                if col == cols - 1:
+                    row += 1
+                    dirc = False
+                elif row == 0:
+                    col += 1
+                    dirc = False
                 else:
-                    # If we are moving downwards and hit the boundary
-                    if row == rows - 1:
-                        col += 1
-                    else:
-                        row += 1
-                direction *= -1  # Change direction
+                    row -= 1
+                    col += 1
+            else:
+                if row == rows - 1:
+                    col += 1
+                    dirc = True
+                elif col == 0:
+                    row += 1
+                    dirc = True
+                else:
+                    row += 1
+                    col -= 1
 
-        return ans
+        return result
