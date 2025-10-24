40285646df6957ed4d2e36e375e2abf0639ab9d4
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-24 18:17:44 +0300

    Time: 2 ms (17.66%) | Memory: 18.1 MB (8.18%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,14 +1,33 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs)
-        city = defaultdict(int)
-        for i in range(n):
-            city[i] = costs[i][1] - costs[i][0]
+        diff = []
+        for i, (v1,v2) in enumerate(costs):
+            diff.append([abs(v1-v2), i])
 
-        city = sorted(city.items(), key=lambda x: x[1])
-        a = n//2
-        min_cost = 0
-        for b in range(n//2):
-            min_cost += costs[city[b][0]][1] + costs[city[a][0]][0]
-            a += 1
-        return min_cost
+        diff.sort(reverse=True)
+        counta = 0
+        countb = 0
+        result = 0
+        
+        for _, i in diff:
+            if costs[i][0] < costs[i][1] and counta < n//2:
+                counta += 1
+                result += costs[i][0]
+
+            elif costs[i][1] < costs[i][0] and countb < n//2:
+                countb += 1
+                result += costs[i][1]
+
+            elif counta < n//2:
+                counta += 1
+                result += costs[i][0]
+
+            elif countb < n//2:
+                countb += 1
+                result += costs[i][1]
+
+            print(counta, countb)
+
+        return result
+
