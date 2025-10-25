7f82eab8abe1c6477bfb606a1eb14bd4032ebc37
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-25 16:55:45 +0300

    Time: 1 ms (35.26%) | Memory: 17.9 MB (47.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -2,16 +2,23 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-
-        max_common = min(len(s) for s in strs) # n - len of strs
-        # create a variable 
-        word = strs[0]
-        count = [] # it counts the logenst common prefix
-
-        # for loop that iterate to all strings in the strs
-        for i in range(max_common):  # m len min string of strs
-            for j in range(len(strs)): # n len strs
-                if word[i] != strs[j][i]:
-                    return ''.join(count)
-            count.append(word[i])
-        return ''.join(count)
+        
+        # Find the minimum length string
+        min_len = min(len(s) for s in strs)
+        
+        # Use first string as reference
+        first_str = strs[0]
+        result = []
+        
+        # Check each character position
+        for i in range(min_len):
+            current_char = first_str[i]
+            
+            # Check if all strings have the same character at position i
+            for j in range(1, len(strs)):  # Start from 1 since first string is reference
+                if strs[j][i] != current_char:
+                    return ''.join(result)
+            
+            result.append(current_char)
+        
+        return ''.join(result)
