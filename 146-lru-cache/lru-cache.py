42a5acb8552f65843f43d8829a4d4faf178d4e4c
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-26 12:36:51 +0300

    Time: 158 ms (33.12%) | Memory: 78.1 MB (43.67%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,38 +1,77 @@
-class LRUCache:
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
+class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = defaultdict(int)
-        self.used = defaultdict(int)
-        self.count = 0
+        self.cache = {}  # key -> Node mapping
+        
+        # Dummy head and tail for easier edge case handling
+        self.head = Node()  # Most recently used
+        self.tail = Node()  # Least recently used
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _add_node(self, node):
+        """Add node right after head (most recent position)"""
+        node.prev = self.head
+        node.next = self.head.next
+        
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def _remove_node(self, node):
+        """Remove a node from the linked list"""
+        prev_node = node.prev
+        next_node = node.next
+        
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    
+    def _move_to_head(self, node):
+        """Move node to head (most recent position)"""
+        self._remove_node(node)
+        self._add_node(node)
+    
+    def _pop_tail(self):
+        """Remove and return the tail node (least recently used)"""
+        node = self.tail.prev
+        self._remove_node(node)
+        return node
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.count += 1
-            self.used[key] = self.count
-            return self.cache[key]
-        else:
+        # If key doesn't exist, return -1
+        if key not in self.cache:
             return -1
+        
+        # Key exists - get the node and move it to head
+        node = self.cache[key]
+        self._move_to_head(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) < self.capacity or key in self.cache:
-            self.cache[key] = value
-            self.used[key] = self.count
+        # If key already exists, update value and move to head
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
         else:
-            lru = key
-            n = float('inf')
-            for k, v in self.used.items():
-                if self.used[k] < n:
-                    lru = k
-                    n = v
-
-            del self.used[lru]
-            del self.cache[lru]         
-            self.cache[key] = value
-            self.used[key] = self.count
-
-        self.count += 1
+            # Create new node
+            new_node = Node(key, value)
             
+            # Add to cache and linked list
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            
+            # If over capacity, remove LRU item
+            if len(self.cache) > self.capacity:
+                lru_node = self._pop_tail()
+                del self.cache[lru_node.key]
+
 
 
 # Your LRUCache object will be instantiated and called as such:
