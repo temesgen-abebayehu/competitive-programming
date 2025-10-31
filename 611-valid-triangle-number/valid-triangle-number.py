9272b14de7981e9aa16f68b2d2f983d9caa145e2
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-01 02:46:39 +0300

    Time: 416 ms (87.48%) | Memory: 17.8 MB (53.97%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -1,12 +1,16 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
-        count = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                curr_sum = nums[i] + nums[j]
-                idx = bisect_left(nums, curr_sum)
-                count += max(0, (idx-j-1))
+        nums.sort()
+        triangles = 0
+        for k in range(n-1, -1, -1):
+            i, j = 0, k-1
+            while i<j:
+                if nums[i] + nums[j] > nums[k]:
+                    triangles += (j-i)
+                    j-=1
+                else:
+                    i+=1
 
-        return count
+
+        return triangles
