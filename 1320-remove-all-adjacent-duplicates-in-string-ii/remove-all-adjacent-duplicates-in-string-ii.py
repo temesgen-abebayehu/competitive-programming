12bf925d4ac91ecd1dc7ab48172755244a57c007
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-10-31 23:13:56 +0300

    Time: 50 ms (31.60%) | Memory: 22.5 MB (54.50%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -2,18 +2,20 @@ class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         stack = []
         for i in range(len(s)):
+            # if stack is not empty and the last value equal to current value
             if stack and stack[-1][0] == s[i]:
                 stack[-1][1] += 1
+
+            # the neighbore are not similar
             else:
                 stack.append([s[i],1])
                 
+            # if we got k consquative char simply remove it
             if stack and stack[-1][1] == k:
                 stack.pop()
             
 
-
         result = []
-        print(stack)
         for ele in stack:
             result.append(ele[0] * ele[1])
 
