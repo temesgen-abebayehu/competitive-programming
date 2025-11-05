c78ead50db40032dc2d2ce09326d0a11e144020c
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-05 18:34:13 +0300

    Time: 103 ms (90.80%) | Memory: 18.2 MB (23.46%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,18 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        dic = defaultdict(int)
+        freq = defaultdict(int)
         max_freq = 0
         ans = 0
         l = 0
 
-        for r  in range(len(s)):
-            dic[s[r]] += 1
-            max_freq = max(max_freq, dic[s[r]])
+        for r in range(len(s)):
+            freq[s[r]] += 1
+            max_freq = max(max_freq, freq[s[r]])
 
-            if (r-l+1) - max_freq > k:
-                dic[s[l]] -= 1
-                l += 1
-
+            if r - max_freq - l >= k:
+                freq[s[l]] -= 1
+                l+=1
             ans = max(ans, r-l+1)
-
+        
         return ans
