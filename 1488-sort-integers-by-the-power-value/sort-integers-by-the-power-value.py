ad19a1b5badb13b42a926317a45286a7a900e0b0
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-06 18:09:50 +0300

    Time: 99 ms (65.34%) | Memory: 34.2 MB (22.28%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -1,23 +1,25 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         memo = {}
-        def dp(x):
-            if x <= 1:
-                return 0
 
-            if x in memo:
-                return memo[x]
+        def count_power(num):
+            if num == 1:
+                return 1
 
-            if x%2==0:
-                memo[x] = 1 + dp(x//2)
+            if num in memo:
+                return memo[num]
+
+            
+            if num % 2 == 0:
+                memo[num] = 1 + count_power(num//2)
             else:
-                memo[x] = 1 + dp(3*x+1)
-            return memo[x]
+                memo[num] = 1 + count_power(3 * num + 1)
 
-        nums = []
-        for x in range(lo, hi+1):
-            power = dp(x)
-            nums.append((power, x))
+            return memo[num]
 
-        nums.sort()
-        return nums[k-1][1]
+        ans = []
+        for num in range(lo, hi+1):
+            ans.append((count_power(num), num))
+
+        ans.sort(key=lambda x: (x[0], x[1]))
+        return ans[k-1][1]
