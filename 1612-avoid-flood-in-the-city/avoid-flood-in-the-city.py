1d64357fe094d6a891483991d4b7a73dd549cf9d
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-06 19:34:02 +0300

    Time: 415 ms (5.35%) | Memory: 34 MB (70.80%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -1,22 +1,31 @@
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        full_lake = {}
-        dry_day = []
-        result  = [-1] * len(rains)
+        # rains = [1,2,0,0,2,1] => (day-> lake)
+        # create dict to traverse day with lake
+        full = {}
+        # create set to traverse dry day
+        dry = []
+        ans = [-1] * len(rains)
+
         for day, lake in enumerate(rains):
+            # it is dry day
             if lake == 0:
-                dry_day.append(day)
-                result[day] = 1
-            else:
-                if lake in full_lake:
-                    d = bisect_right(dry_day, full_lake[lake])
-                    if d<len(dry_day) and dry_day[d] < day:
-                        result[dry_day[d]] = lake
-                        dry_day.pop(d)
-                    else:
-                        return []
-                full_lake[lake] = day
-
-
-        return result
+                dry.append(day)
+                continue
             
+            # it should dry to avoide flood
+            if lake in full:
+                idx = bisect.bisect_right(dry, full[lake])
+                if idx == len(dry):
+                    return []
+
+                ans[dry[idx]] = lake
+                dry.pop(idx)
+
+            # mark the lake already full
+            full[lake] = day
+
+        for day in dry:
+            ans[day] = 1
+        
+        return ans      
