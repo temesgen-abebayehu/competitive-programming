a8018628474498113027e6976031146c2416d7ed
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-06 16:48:17 +0300

    Time: 0 ms (100.00%) | Memory: 18.8 MB (27.05%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,13 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        h = len(nums) - 1
-        while l <= h:
-            m = (l + h) // 2
-            if nums[m] == target:
-                return m
-            elif nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
-                h = m - 1
-        return -1
+        idx = bisect_left(nums, target)
+        if idx < len(nums) and nums[idx] == target:
+            return idx
+        else:
+            return -1
