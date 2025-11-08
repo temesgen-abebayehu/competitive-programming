d0683c9f21d21d9a67c2f108be269bfdd0f91209
Temesgen abebayehu <130055841+temesgen-abebayehu@users.noreply.github.com>
2025-11-09 02:20:06 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.72%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,27 +1,16 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs)
-        sorted_cost = sorted(costs, key=lambda x: abs(x[0]-x[1]), reverse=True)
+        sorted_cost = sorted(costs, key=lambda x: (x[0]-x[1]))
 
-        counta = 0
-        countb = 0
+        count = 0
         result = 0
         
         for ca, cb in sorted_cost:
-            if (ca < cb and counta < n//2):
-                counta += 1
+            if count < n//2:
                 result += ca
-
-            elif cb < ca and countb < n//2:
-                countb += 1
-                result += cb
-
-            elif counta < n//2:
-                counta += 1
-                result += ca
-
-            elif countb < n//2:
-                countb += 1
+                count += 1
+            else:
                 result += cb
 
         return result
